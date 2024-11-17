25dfb068b598aeb4140ce0823a1631a5dd838035
MohitAhir102 <124023028+MohitAhir102@users.noreply.github.com>
2024-11-17 12:17:46 +0530

    folder stcture and add some class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUpPage from "./components/SignUpPage";
+import SignUpPage from "./components/Login_page/SignUpPage";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
 import Desk from "./components/Destop 2/Desk";
 
