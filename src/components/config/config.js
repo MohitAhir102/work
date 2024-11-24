670cc066756332021207da1dfddedc6a7263f6d5
MohitAhir102 <124023028+MohitAhir102@users.noreply.github.com>
2024-11-24 21:59:51 +0530

    change key

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,8 +1,6 @@
-export const TWITTER_API_KEY = "LG3MUHgc9OFDeCRzVXsX0dchj";
-export const TWITTER_API_SECRET =
-  "Gvk2Ct9SdGrdSxcQ5nMJli78op2nXGLEVogPyyyOiIQ9ybdypu";
+export const TWITTER_API_KEY = "Q5M78kEqqxJK2k3NVQtrM7vs6";
+export const TWITTER_API_SECRET ="ndBw2FiV8K9q5ZSfOQMmRZ7RNkoTGDVRsNTVCI0Lt02YOi4X8g";
 export const TWITTER_API_CLIENT_ID = "cUloUTgyVjNITVJ4X3NkOF82ejQ6MTpjaQ";
-export const REDIRECT_URI =
-  "https://work-three-tan.vercel.app/leaderboard";
+export const REDIRECT_URI ="https://work-three-tan.vercel.app/leaderboard";
 
   
