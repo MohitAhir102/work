921b2027edea486710d4497573dcf3cafe8bbcd1
MohitAhir102 <124023028+MohitAhir102@users.noreply.github.com>
2024-11-24 21:28:38 +0530

    add
    addd

--- my-app/src/components/config/config.js
+++
@@ -1,4 +0,0 @@
-export const TWITTER_API_KEY = 'LG3MUHgc9OFDeCRzVXsX0dchj';
-export const TWITTER_API_SECRET = 'Gvk2Ct9SdGrdSxcQ5nMJli78op2nXGLEVogPyyyOiIQ9ybdypu';
-export const TWITTER_API_CLIENT_ID = 'cUloUTgyVjNITVJ4X3NkOF82ejQ6MTpjaQ';
-export const REDIRECT_URI = 'http://localhost:3000/leaderboard';
